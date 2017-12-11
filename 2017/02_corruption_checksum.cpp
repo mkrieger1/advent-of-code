0430f4baa2aefc097982bf447dc4a70add50eecc
mkrieger1 <mkrieger1@users.noreply.github.com>
2017-12-11 02:11:42 +0100

    solve part 2 of day 2

--- 2017/02_corruption_checksum.cpp
+++ 2017/02_corruption_checksum.cpp
@@ -2,6 +2,7 @@
 #include <iostream>
 #include <iterator>
 #include <sstream>
+#include <set>
 #include <vector>
 
 // A row in the spreadsheet consists of a list of numbers.
@@ -25,6 +26,27 @@ public:
         auto max{std::max_element(std::begin(numbers), std::end(numbers))};
         auto min{std::min_element(std::begin(numbers), std::end(numbers))};
         return *max - *min;
+    }
+
+    // Return the ratio between the only two evenly divisible numbers.
+    // Assumes that there exists only one such pair of numbers.
+    // Assumes that the row doesn't contain the same number more than once.
+    int evenly_divisible_ratio() {
+        if (numbers.empty()) { return 0; }
+
+        std::set<int> nums{std::begin(numbers), std::end(numbers)};
+
+        // guard against no pair of evenly divisible numbers exists
+        auto max{std::max_element(std::begin(nums), std::end(nums))};
+
+        for (auto x : nums) {
+            for (int i = 2; i * x <= *max; ++i) {
+                if (nums.find(i * x) != std::end(nums)) {
+                    return i;
+                }
+            }
+        }
+        return 0;
     }
 };
 
@@ -50,11 +72,21 @@ public:
         }
         return result;
     }
+
+    // Return the sum of the ratio between the two evenly divisible numbers in
+    // each row.
+    int sum_rows_evenly_divisible_ratio() {
+        int result{0};
+        for (auto row : rows) {
+            result += row.evenly_divisible_ratio();
+        }
+        return result;
+    }
 };
 
 
 int main() {
     Spreadsheet sheet;
     std::cin >> sheet;
-    std::cout << sheet.sum_rows_max_difference() << '\n';
+    std::cout << sheet.sum_rows_evenly_divisible_ratio() << '\n';
 }
