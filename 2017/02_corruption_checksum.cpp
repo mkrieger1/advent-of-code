34fc180ba788a298e97284895fb72a4085bf8f18
mkrieger1 <mkrieger1@users.noreply.github.com>
2017-12-11 01:26:55 +0100

    remove unused functions and includes again

--- 2017/02_corruption_checksum.cpp
+++ 2017/02_corruption_checksum.cpp
@@ -1,20 +1,11 @@
 #include <iostream>
 #include <iterator>
-#include <string>
 #include <sstream>
 #include <vector>
 
 // A row in the spreadsheet consists of a list of numbers.
 class Row {
     std::vector<int> numbers;
-
-    friend std::ostream& operator<<(std::ostream& output, const Row& row) {
-        std::copy(
-            std::begin(row.numbers), std::end(row.numbers),
-            std::ostream_iterator<int>{output, " "}
-        );
-        return output;
-    }
 
 public:
     // Create a row from a string of whitespace-delimited numbers.
@@ -40,23 +31,11 @@ class Spreadsheet {
         return input;
     }
 
-    friend std::ostream&
-    operator<<(std::ostream& output, const Spreadsheet& sheet) {
-        std::copy(
-            std::begin(sheet.rows), std::end(sheet.rows),
-            std::ostream_iterator<Row>{output, "\n"}
-        );
-        return output;
-    }
-
 public:
-    int size() { return rows.size(); }
 };
 
 
 int main() {
     Spreadsheet sheet;
     std::cin >> sheet;
-    std::cout << "The spreadsheet has " << sheet.size() << " rows:\n";
-    std::cout << sheet << '\n';
 }
