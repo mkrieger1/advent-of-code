453b3978b90a2f5158daaafef9fd52c8b36fe029
mkrieger1 <mkrieger1@users.noreply.github.com>
2017-12-23 23:33:15 +0100

    add a solution to Day 3 Part 1

---
+++ 2017/03_spiral_memory.cpp
@@ -0,0 +1,64 @@
+#include <cmath>
+#include <iostream>
+
+// Location:
+// 17 16 15 14 13
+// 18  5  4  3 12
+// 19  6  1  2 11
+// 20  7  8  9 10
+// 21 22 23 24 25
+
+// Radius:
+//  2  2  2  2  2
+//  2  1  1  1  2
+//  2  1  0  1  2
+//  2  1  1  1  2
+//  2  2  2  2  2
+
+// Angle:
+//  2  1  0  1  2
+//  1  1  0  1  1
+//  0  0  0  0  0
+//  1  1  0  1  1
+//  2  1  0  1  2
+
+// Distance = Radius + Angle
+
+// Return the number of locations within the radius.
+// 0 -> 1,  1 -> 9,  2 -> 25,  ...
+int num_locations(int radius) {
+    return std::pow(2 * radius + 1, 2);
+}
+
+// Return the radius of the location in the spiral memory.
+int radius(int location) {
+    int radius{0};
+    while (num_locations(radius) < location) {
+        ++radius;
+    }
+    return radius;
+}
+
+// Return the angle of the location in the spiral memory.
+// For a location with radius R, its angle is between 0 and R.
+// The first location (*) with radius R+1 is to the right of the last location
+// with radius R, so its angle is R.
+int angle(int location) {
+    int r{radius(location)};
+    if (r == 0) { return 0; }
+    int num{num_locations(r - 1)};
+    int rest{location - num};
+    return std::abs(rest % (2 * r) - r);
+}
+
+// Return the Manhattan distance from the memory location to the access port
+// (at location 1).
+int distance(int location) {
+    return radius(location) + angle(location);
+}
+
+int main() {
+    int location;
+    std::cin >> location;
+    std::cout << distance(location) << '\n';
+}
