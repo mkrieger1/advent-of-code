0e917cc3e28a8a8ece5bcbcface5fe0a15d08962
mkrieger1 <mkrieger1@users.noreply.github.com>
2018-01-01 18:39:23 +0100

    rename test cases

--- 2017/03/main.cpp
+++ 2017/03/main.cpp
@@ -4,27 +4,27 @@
 #include <iostream>
 #include <vector>
 
-struct TestCase {
-    Location location;
+struct TestCaseDistance {
+    Location::Address address;
     Location::Distance distance;
 };
 
 int main()
 {
-    std::vector<TestCase> tests{
-        {{1}, 0},
-        {{9}, 2},
-        {{10}, 3},
-        {{11}, 2},
-        {{12}, 3},
-        {{13}, 4},
-        {{14}, 3},
-        {{23}, 2},
-        {{1024}, 31}
+    std::vector<TestCaseDistance> tests_distance{
+        {1, 0},
+        {9, 2},
+        {10, 3},
+        {11, 2},
+        {12, 3},
+        {13, 4},
+        {14, 3},
+        {23, 2},
+        {1024, 31}
     };
 
-    for (auto const& test : tests) {
-        assert(test.location.distance_origin() == test.distance);
+    for (auto const& test : tests_distance) {
+        assert(Location{test.address}.distance_origin() == test.distance);
     }
 
     Location::Address a;
