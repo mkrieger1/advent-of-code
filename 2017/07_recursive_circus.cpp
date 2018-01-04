729d55f18c97a226981523cb408a17fe56232d43
mkrieger1 <mkrieger1@users.noreply.github.com>
2018-01-04 23:15:33 +0100

    add a Program class which wraps reading from a line

--- 2017/07_recursive_circus.cpp
+++ 2017/07_recursive_circus.cpp
@@ -1,36 +1,58 @@
 #include <iostream>
 #include <regex>
+#include <stdexcept>
 #include <vector>
+
+class Program {
+    struct InvalidFormat : public std::runtime_error {
+        InvalidFormat() : std::runtime_error("Line has invalid format.") {};
+    };
+
+public:
+    Program(const std::string& line)
+    {
+        std::smatch line_match;
+        if (!std::regex_match(line, line_match,
+                std::regex{R"((\w+) \((\d+)\)(?: -> ((?:\w+(, |$))+))?)"}
+        )) throw InvalidFormat{};
+
+        name_ = line_match[1];
+        weight_ = std::stoi(line_match[2]);
+
+        auto names{line_match[3]};
+        if (!names.length()) return;
+
+        auto names_str{names.str()};
+        std::smatch names_match;
+        while (std::regex_search(names_str, names_match,
+                std::regex{R"(\w+)"}
+        )) {
+            supported_.push_back(names_match.str());
+            names_str = names_match.suffix();
+        }
+    }
+
+    void print()
+    {
+        std::cout << "name: " << name_ << '\n';
+        std::cout << "weight: " << weight_ << '\n';
+        std::cout << "supports:\n";
+        for (auto const& name : supported_) {
+            std::cout << name << '\n';
+        }
+    }
+
+private:
+    std::string name_;
+    int weight_;
+    std::vector<std::string> supported_;
+};
 
 int main()
 {
     std::string line;
     while(std::getline(std::cin, line)) {
-        std::regex outer{R"((\w+) \((\d+)\)(?: -> ((?:\w+(, |$))+))?)"};
-        std::regex inner{R"(\w+)"};
-
-        std::smatch match;
-        if (!std::regex_match(line, match, outer)) continue;
-
-        std::string name{match[1]};
-        int weight{std::stoi(match[2])};
-        std::cout << "name: " << name << '\n';
-        std::cout << "weight: " << weight << '\n';
-
-        auto rest{match[3].str()};
-        if (!rest.length()) continue;
-
-        std::cout << "rest: " << rest << '\n';
-
-        std::vector<std::string> names;
-        std::smatch subname;
-        while (std::regex_search(rest, subname, inner)) {
-            names.push_back(subname.str());
-            rest = subname.suffix();
-        }
-
-        for (auto const& n : names) {
-            std::cout << n << '\n';
-        }
+        Program prog{line};
+        prog.print();
     }
 }
