eb3e044a1662dc053cf5c585686c0e2f2c6aa8de
mkrieger1 <mkrieger1@users.noreply.github.com>
2018-01-07 01:01:23 +0100

    move the functions into a new ProgramTower class

--- 2017/07_recursive_circus.cpp
+++ 2017/07_recursive_circus.cpp
@@ -59,101 +59,129 @@ private:
     std::vector<std::string> supported_;
 };
 
-using ProgramTower = std::unordered_map<std::string, Program>;
+class ProgramTower {
+    // map name -> program with that name
+    using NameMap = std::unordered_map<std::string, Program>;
+    // map program name -> name of supporting program
+    using SupportMap = std::unordered_map<std::string, std::string>;
 
-// Return map: program name -> name of supporting program
-std::unordered_map<std::string, std::string>
-supporter_map(const ProgramTower& programs)
-{
-    std::unordered_map<std::string, std::string> result;
-    for (auto const& name_prog : programs) {
-        auto supporter{name_prog.second};
-        for (auto const& supported : supporter.supported()) {
-            result[supported] = supporter.name();
+public:
+    ProgramTower() = default;
+    ProgramTower(const std::vector<Program>&); // TODO?
+
+    friend std::istream& operator>>(std::istream& input, ProgramTower& tower)
+    {
+        // read all programs
+        Program prog;
+        while (input >> prog) {
+            tower.programs_.insert({prog.name(), prog});
         }
-    }
-    return result;
-}
 
-// Return total weight of the program with the given name, and all sub-towers
-// it is supporting.
-int total_weight(const ProgramTower& programs, const std::string& name)
-{
-    auto base{programs.at(name)};
-    int result{base.weight()};
-    for (auto const& sub : base.supported()) {
-        result += total_weight(programs, sub);
-    }
-    return result;
-}
-
-// Determing which program in the tower supported by the program with the given
-// name has the wrong weight (i.e. causes a sub-tower to be unbalanced).
-// Assumes that there is exactly one such program.
-struct SearchResult {
-    bool is_balanced;
-    std::string name;
-    int correct_weight;
-};
-
-SearchResult wrong_weight(const ProgramTower& programs, const std::string& name)
-{
-    std::unordered_map<int, std::vector<std::string>> weights;
-    for (auto const& sub : programs.at(name).supported()) {
-        weights[total_weight(programs, sub)].push_back(sub);
-    }
-
-    if (weights.size() == 1) return {true, "", 0}; // name is balanced
-
-    int correct_total_weight;
-    std::string wrong_program;
-
-    for (auto const& weight_subs : weights) {
-        auto subs{weight_subs.second};
-        if (subs.size() != 1) {
-            correct_total_weight = weight_subs.first;
-            continue;
-        }
-        // subs contains single sub-program with the wrong total weight
-        // if the sub-towers are balanced, sub itself is wrong
-        for (auto const& sub : subs) {
-            auto result{wrong_weight(programs, sub)};
-            if (!result.is_balanced) {
-                return result;
-            } else {
-                wrong_program = sub;
-                continue;
+        // build map of which program is supported by which
+        for (auto const& name_prog : tower.programs_) {
+            auto supporter{name_prog.second};
+            for (auto const& supported : supporter.supported()) {
+                tower.support_.insert({supported, supporter.name()});
             }
         }
+
+        // find base program (the one which is not supported by any other)
+        for (auto const& name_prog : tower.programs_) {
+            auto name{name_prog.first};
+            if (tower.support_.find(name) == std::end(tower.support_)) {
+                tower.base_ = name;
+            }
+        }
+
+        return input;
     }
-    auto correct_weight{
-        programs.at(wrong_program).weight()
-        + correct_total_weight - total_weight(programs, wrong_program)
+
+    // Return the total weight of the program with the given name, and all
+    // sub-towers it is supporting.
+    int total_weight(const std::string& name)
+    {
+        auto base{programs_.at(name)};
+        int result{base.weight()};
+        for (auto const& sub : base.supported()) {
+            result += total_weight(sub);
+        }
+        return result;
+    }
+
+    // Determine which program in the tower supported by the program with the
+    // given name has the wrong weight (i.e. causes a sub-tower to be
+    // unbalanced).  Assumes that there is exactly one such program.
+    struct SearchResult {
+        bool is_balanced;
+        std::string name;
+        int correct_weight;
     };
-    return {false, wrong_program, correct_weight};
-}
+
+    SearchResult wrong_weight(const std::string& name)
+    {
+        std::unordered_map<int, std::vector<std::string>> weights;
+        for (auto const& sub : programs_.at(name).supported()) {
+            weights[total_weight(sub)].push_back(sub);
+        }
+
+        if (weights.size() == 1) return {true, "", 0}; // name is balanced
+
+        int correct_total_weight;
+        std::string wrong_program;
+
+        for (auto const& weight_subs : weights) {
+            auto subs{weight_subs.second};
+            if (subs.size() != 1) {
+                correct_total_weight = weight_subs.first;
+                continue;
+            }
+            // subs contains single sub-program with the wrong total weight
+            // if the sub-towers are balanced, sub itself is wrong
+            for (auto const& sub : subs) {
+                auto result{wrong_weight(sub)};
+                if (!result.is_balanced) {
+                    return result;
+                } else {
+                    wrong_program = sub;
+                    continue;
+                }
+            }
+        }
+        auto correct_weight{
+            programs_.at(wrong_program).weight()
+            + correct_total_weight - total_weight(wrong_program)
+        };
+        return {false, wrong_program, correct_weight};
+    }
+
+    std::string base() const { return base_; }
+
+private:
+    std::string base_;
+    NameMap programs_;
+    SupportMap support_;
+};
+
 
 int main()
 {
-    ProgramTower programs_by_name;
-    Program prog;
-    while (std::cin >> prog) {
-        programs_by_name.insert({prog.name(), prog});
-    }
+    ProgramTower tower;
+    std::cin >> tower;
+    auto name{tower.base()};
 
-    auto supporters{supporter_map(programs_by_name)};
-    for (auto const& name_prog : programs_by_name) {
-        auto name{name_prog.first};
-        if (supporters.find(name) != std::end(supporters)) continue;
-        std::cout << name << " not supported by any program!\n";
-        std::cout << "The total weight supported by " << name
-                  << " is " << total_weight(programs_by_name, name) << '\n';
+    std::cout << "Program '" << name
+              << "' is the base of the tower of programs.\n";
 
-        auto wrong{wrong_weight(programs_by_name, name)};
-        std::cout << "The sub-tower supported by " << name << " is "
-                  << (wrong.is_balanced ? "" : "not ")
-                  << "balanced\n";
-        std::cout << "Program " << wrong.name << " has the wrong weight.\n";
-        std::cout << "Its weight would need to be " << wrong.correct_weight << '\n';
-    }
+    std::cout << "The total weight supported by '" << name
+              << "' is " << tower.total_weight(name) << ".\n";
+
+    auto wrong{tower.wrong_weight(name)};
+    std::cout << "The sub-tower supported by '" << name << "' is "
+              << (wrong.is_balanced ? "" : "not ")
+              << "balanced.\n";
+
+    std::cout << "Program '" << wrong.name << "' has the wrong weight.\n";
+
+    std::cout << "Its weight would need to be "
+              << wrong.correct_weight << ".\n";
 }
