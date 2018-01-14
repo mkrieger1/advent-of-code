e14d10c5c009001ae67be84b51a3e44f8a34006d
mkrieger1 <mkrieger1@users.noreply.github.com>
2018-01-14 21:58:14 +0100

    add minimal code to make test pass

--- 2017/08/register_machine.cpp
+++ 2017/08/register_machine.cpp
@@ -1,6 +1,14 @@
 #include "register_machine.h"
 
+Instruction::Instruction(const std::string& line)
+{
+}
+
 RegisterMachine::RegisterValue RegisterMachine::max_register_value() const
 {
     return 0;
+}
+
+void RegisterMachine::execute(const Instruction& instruction)
+{
 }
--- 2017/08/register_machine.h
+++ 2017/08/register_machine.h
@@ -1,5 +1,12 @@
+#include <string>
+
+struct Instruction {
+    Instruction(const std::string& line);
+};
+
 class RegisterMachine {
 public:
     using RegisterValue = int;
     RegisterValue max_register_value() const;
+    void execute(const Instruction&);
 };
