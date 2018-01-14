5897cc8f2734145f72b53e2a33ca006aeac8952f
mkrieger1 <mkrieger1@users.noreply.github.com>
2018-01-14 22:08:15 +0100

    add new test

--- 2017/08/test.cpp
+++ 2017/08/test.cpp
@@ -1,14 +1,22 @@
 #include "register_machine.h"
 #include <cassert>
+#include <vector>
 
 void run_tests()
 {
     RegisterMachine machine{};
     assert(machine.max_register_value() == 0);
 
-    Instruction instruction{"b inc 5 if a > 1"};
-    machine.execute(instruction);
+    const std::vector<Instruction> instructions{
+        {"b inc 5 if a > 1"},
+        {"a inc 1 if b < 5"}
+    };
+
+    machine.execute(instructions[0]);
     assert(machine.max_register_value() == 0);
+
+    machine.execute(instructions[1]);
+    assert(machine.max_register_value() == 1);
 }
 
 int main()
