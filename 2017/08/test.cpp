199b9a173a1a080f61fdc978b5f0aeb7cd35de83
mkrieger1 <mkrieger1@users.noreply.github.com>
2018-01-14 21:58:03 +0100

    add new test case

--- 2017/08/test.cpp
+++ 2017/08/test.cpp
@@ -5,6 +5,10 @@ void run_tests()
 {
     RegisterMachine machine{};
     assert(machine.max_register_value() == 0);
+
+    Instruction instruction{"b inc 5 if a > 1"};
+    machine.execute(instruction);
+    assert(machine.max_register_value() == 0);
 }
 
 int main()
