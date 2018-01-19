7e797ef5e06d57599c7e4b79883075d661f75975
mkrieger1 <mkrieger1@users.noreply.github.com>
2018-01-20 00:28:00 +0100

    use const in catch

--- 2017/08/main.cpp
+++ 2017/08/main.cpp
@@ -7,7 +7,7 @@ int main()
     Machine::Instruction i;
     try {
         while (std::cin >> i) m.execute(i);
-    } catch (Machine::Instruction::ParseError& e) {
+    } catch (const Machine::Instruction::ParseError& e) {
         std::cerr << e.what() << '\n';
     }
     std::cout << m.max_value() << '\n';
