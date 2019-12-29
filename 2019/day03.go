b3873ec08c44a91def7003629c4aafbf42ba7e05
mkrieger1 <mkrieger1@users.noreply.github.com>
2019-12-29 22:42:24 +0100

    Add segment method for calculating a crossing

--- 2019/day03.go
+++ 2019/day03.go
@@ -33,6 +33,47 @@ type segment struct {
 	length int
 }
 
+// crosses returns the point where seg crosses the other segment.
+func (seg segment) crosses(other segment) (*point, error) {
+	var vert, hor segment
+	if seg.start.x == seg.end.x { // seg vertical
+		if other.start.x == other.end.x {
+			return nil, nil // both vertical
+		}
+		vert, hor = seg, other
+	} else if seg.start.y == seg.end.y { // seg horizontal
+		if other.start.y == other.end.y {
+			return nil, nil // both horizontal
+		}
+		vert, hor = other, seg
+	} else {
+		return nil, fmt.Errorf("Segment not horizontal or vertical")
+	}
+
+	x := vert.start.x
+	var xlow, xhigh int
+	if hor.start.x < hor.end.x {
+		xlow, xhigh = hor.start.x, hor.end.x
+	} else {
+		xlow, xhigh = hor.end.x, hor.start.x
+	}
+	if (x < xlow) || (x > xhigh) {
+		return nil, nil // no crossing
+	}
+
+	y := hor.start.y
+	var ylow, yhigh int
+	if vert.start.y < vert.end.y {
+		ylow, yhigh = vert.start.y, vert.end.y
+	} else {
+		ylow, yhigh = vert.end.y, vert.start.y
+	}
+	if (y < ylow) || (y > yhigh) {
+		return nil, nil // no crossing
+	}
+	return &point{x, y}, nil
+}
+
 // parseSegment converts a string description of a wire segment
 // to its direction and length.
 func parseSegment(seg string) (direction, int, error) {
@@ -100,48 +141,17 @@ func MostCentralCrossing(wires [2][]string) (int, error) {
 	best := -1
 	for _, seg1 := range segments[0] {
 		for _, seg2 := range segments[1] {
-			var vert, hor segment
-			if seg1.start.x == seg1.end.x { // segment1 vertical
-				if seg2.start.x == seg2.end.x {
-					continue // both vertical
-				}
-				vert, hor = seg1, seg2
-			} else if seg1.start.y == seg1.end.y { // segment1 horizontal
-				if seg2.start.y == seg2.end.y {
-					continue // both horizontal
-				}
-				vert, hor = seg2, seg1
-			} else {
-				return 0, fmt.Errorf("Segment not horizontal or vertical")
+			crossing, err := seg1.crosses(seg2)
+			if err != nil {
+				return 0, err
 			}
-
-			x := vert.start.x
-			var xlow, xhigh int
-			if hor.start.x < hor.end.x {
-				xlow, xhigh = hor.start.x, hor.end.x
-			} else {
-				xlow, xhigh = hor.end.x, hor.start.x
+			if crossing == nil {
+				continue
 			}
-			if (x < xlow) || (x > xhigh) {
-				continue // no crossing
-			}
-
-			y := hor.start.y
-			var ylow, yhigh int
-			if vert.start.y < vert.end.y {
-				ylow, yhigh = vert.start.y, vert.end.y
-			} else {
-				ylow, yhigh = vert.end.y, vert.start.y
-			}
-			if (y < ylow) || (y > yhigh) {
-				continue // no crossing
-			}
-
-			// crossing at x, y
-			if (x == 0) && (y == 0) {
+			if (crossing.x == 0) && (crossing.y == 0) {
 				continue // doesn't count
 			}
-			dist := point{x, y}.manhattanDistance()
+			dist := crossing.manhattanDistance()
 			if (best == -1) || (dist < best) {
 				best = dist
 			}
