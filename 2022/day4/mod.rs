0692d6caab625301a2442741f42449ae9ece979e
mkrieger1 <mkrieger1@users.noreply.github.com>
2023-01-01 21:27:45 +0100

    Solve day 4 part 2
    And remove redundant `test_` prefix from test functions.

--- 2022/day4/mod.rs
+++ 2022/day4/mod.rs
@@ -10,6 +10,12 @@ impl Range {
     pub fn contains(&self, other: &Range) -> bool {
         self.begin <= other.begin && self.end >= other.end
     }
+
+    pub fn overlaps(&self, other: &Range) -> bool {
+        self.begin <= other.begin && self.end >= other.begin
+            || self.begin <= other.end && self.end >= other.end
+            || other.contains(&self)
+    }
 }
 
 fn parse_range(input: &str) -> Option<Range> {
@@ -36,6 +42,11 @@ fn one_range_contains_other(input: &str) -> Option<bool> {
     Some(first.contains(&second) || second.contains(&first))
 }
 
+fn ranges_overlap(input: &str) -> Option<bool> {
+    let (first, second) = parse_ranges(input)?;
+    Some(first.overlaps(&second))
+}
+
 pub fn cleanup_part1<I>(input: I) -> i32
 where
     I: IntoIterator,
@@ -54,7 +65,10 @@ where
     I: IntoIterator,
     I::Item: Borrow<str>,
 {
-    todo!()
+    input
+        .into_iter()
+        .map(|line| ranges_overlap(line.borrow()).unwrap_or_default() as i32)
+        .sum()
 }
 
 #[cfg(test)]
@@ -66,25 +80,47 @@ mod tests {
     ];
 
     #[test]
-    fn test_cleanup_part1() {
+    fn part1_example() {
         assert_eq!(cleanup_part1(EXAMPLE), 2);
     }
 
     #[test]
-    fn test_contains() {
-        assert!(
-            Range { begin: 2, end: 8 }.contains(&Range { begin: 3, end: 7 })
-        );
-        assert!(
-            Range { begin: 4, end: 6 }.contains(&Range { begin: 6, end: 6 })
-        );
+    fn contains_example() {
+        assert_eq!(one_range_contains_other(EXAMPLE[0]), Some(false));
+        assert_eq!(one_range_contains_other(EXAMPLE[1]), Some(false));
+        assert_eq!(one_range_contains_other(EXAMPLE[2]), Some(false));
+        assert_eq!(one_range_contains_other(EXAMPLE[3]), Some(true));
+        assert_eq!(one_range_contains_other(EXAMPLE[4]), Some(true));
+        assert_eq!(one_range_contains_other(EXAMPLE[5]), Some(false));
     }
 
     #[test]
-    fn test_parse() {
+    fn parse() {
         assert_eq!(
             parse_ranges(EXAMPLE[0]),
             Some((Range { begin: 2, end: 4 }, Range { begin: 6, end: 8 }))
         )
+    }
+
+    #[test]
+    fn part2_example() {
+        assert_eq!(cleanup_part2(EXAMPLE), 4);
+    }
+
+    #[test]
+    fn overlap_example() {
+        assert_eq!(ranges_overlap(EXAMPLE[0]), Some(false));
+        assert_eq!(ranges_overlap(EXAMPLE[1]), Some(false));
+        assert_eq!(ranges_overlap(EXAMPLE[2]), Some(true));
+        assert_eq!(ranges_overlap(EXAMPLE[3]), Some(true));
+        assert_eq!(ranges_overlap(EXAMPLE[4]), Some(true));
+        assert_eq!(ranges_overlap(EXAMPLE[5]), Some(true));
+        assert_eq!(ranges_overlap(EXAMPLE[5]), Some(true));
+    }
+
+    #[test]
+    fn overlap_if_contained() {
+        assert!(ranges_overlap("7-96,6-99").unwrap());
+        assert!(ranges_overlap("6-96,7-95").unwrap());
     }
 }
