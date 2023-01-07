f4491a68b431a66f62dcce2af440d0c53cf0f670
mkrieger1 <mkrieger1@users.noreply.github.com>
2023-01-07 01:20:01 +0100

    Remove Result from day 1

--- 2022/day1/mod.rs
+++ 2022/day1/mod.rs
@@ -1,6 +1,6 @@
 use std::io;
 
-fn read_elves<B>(input: B) -> Result<Vec<Vec<i32>>, io::Error>
+fn read_elves<B>(input: B) -> Vec<Vec<i32>>
 where
     B: io::BufRead,
 {
@@ -8,7 +8,10 @@ where
     let mut elf: Vec<i32> = Vec::new();
 
     for line in input.lines() {
-        let line = line?;
+        let line = match line {
+            Err(_) => break,
+            Ok(line) => line,
+        };
         let line = line.trim();
         if line.is_empty() {
             if !elf.is_empty() {
@@ -21,25 +24,25 @@ where
         }
     }
     elves.push(elf.clone());
-    Ok(elves)
+    elves
 }
 
-pub fn max_elf_calories<B>(input: B) -> Result<i32, io::Error>
+pub fn max_elf_calories<B>(input: B) -> i32
 where
     B: io::BufRead,
 {
-    Ok(read_elves(input)?
+    read_elves(input)
         .iter()
         .map(|elf| elf.iter().sum::<i32>())
         .max()
-        .unwrap_or(0))
+        .unwrap_or(0)
 }
 
-pub fn top_3_elves_calories<B>(input: B) -> Result<i32, io::Error>
+pub fn top_3_elves_calories<B>(input: B) -> i32
 where
     B: io::BufRead,
 {
-    let elves = read_elves(input)?;
+    let elves = read_elves(input);
     let top_calories = {
         let mut calories: Vec<i32> =
             elves.iter().map(|elf| elf.iter().sum()).collect();
@@ -47,7 +50,7 @@ where
         calories.reverse();
         calories
     };
-    Ok(top_calories.iter().take(3).sum())
+    top_calories.iter().take(3).sum()
 }
 
 #[cfg(test)]
@@ -73,11 +76,11 @@ mod tests {
 
     #[test]
     fn part1_example() {
-        assert_eq!(max_elf_calories(EXAMPLE.as_bytes()).unwrap(), 24000);
+        assert_eq!(max_elf_calories(EXAMPLE.as_bytes()), 24000);
     }
 
     #[test]
     fn part2_example() {
-        assert_eq!(top_3_elves_calories(EXAMPLE.as_bytes()).unwrap(), 45000);
+        assert_eq!(top_3_elves_calories(EXAMPLE.as_bytes()), 45000);
     }
 }
--- 2022/day1/part2.rs
+++ 2022/day1/part2.rs
@@ -1,9 +1,7 @@
-use std::error::Error;
 use std::io;
 
 use aoc::day1::top_3_elves_calories;
 
-fn main() -> Result<(), Box<dyn Error>> {
-    println!("{}", top_3_elves_calories(io::stdin().lock())?);
-    Ok(())
+fn main() {
+    println!("{}", top_3_elves_calories(io::stdin().lock()));
 }
